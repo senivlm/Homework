e3b397d39f087628e254b3ed3514e6b65e9bbdea
senivlm <91743906+senivlm@users.noreply.github.com>
2022-11-01 13:54:23 +0200

    Update Program.cs

--- Homework_1_Prokopenko_Olexiy/Homework_1_Prokopenko/Program.cs
+++ Homework_1_Prokopenko_Olexiy/Homework_1_Prokopenko/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Channels;
 using Homework_1_Prokopenko;
+// Вітаю. Все доступно.
 
 var allProducts = new List<Product>
 {
@@ -11,4 +12,5 @@ buy.Products.Add(allProducts[0], 3);
 buy.Products.Add(allProducts[1], 2);
 Check.Display(buy);
 Check check = new Check();
+// явний виклик методу переведення в стрічку можна не писати.
 Console.WriteLine(check.ToString());
